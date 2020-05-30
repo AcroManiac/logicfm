626b3d4831c0c4f50682ef833242557a68039029
Artem Orlov <ahamtat108@gmail.com>
2020-05-30 21:48:00 +0300

    Deleted client API

--- scripts/gen-swagger.sh
+++ scripts/gen-swagger.sh
@@ -6,9 +6,8 @@ if [ $retVal -ne 0 ]; then
   echo "Builder API Swagger YAML is not valid"
   exit $retVal
 else
-  swagger generate client -A builderapi -f ../api/builder-api-swagger.yaml -t ../pkg/ \
-    --client-package=builderapiclient
-  swagger generate server -A builderapi -f ../api/builder-api-swagger.yaml -t ../internal/ \
-    --skip-models --existing-models=../pkg/models \
-    --exclude-main
+#  swagger generate client -A builderapi -f ../api/builder-api-swagger.yaml -t ../pkg/ \
+#    --client-package=builderapiclient
+  swagger generate server -f ../api/builder-api-swagger.yaml -t ../internal/api/builder/ \
+    --exclude-main # -A builderapi
 fi
